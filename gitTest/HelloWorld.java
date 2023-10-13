c26532167a9ca76e636d8bf4d0c28b957732c4ba
JB <plum3526@gmail.com>
2023-10-13 16:36:39 +0900

    add HelloWorld.java  test added  test2 added`

--- gitTest/HelloWorld.java
+++ gitTest/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
         System.out.println("hello World");
         System.out.println("HELLO WORLD");
         System.out.println("test1");
+        System.out.println("test2");
     }
 }
